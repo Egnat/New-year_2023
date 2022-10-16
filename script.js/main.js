3941b863e41186c04f7cc425653ad6597f1fd68c
Egnat <sergey_p_i@mail.ru>
2022-10-16 22:52:18 +0300

    (master)

--- script.js/main.js
+++ script.js/main.js
@@ -34,11 +34,11 @@ btnNext.addEventListener("click", () => {positionSlides(1);});
 const articleText = document.querySelector(".slide-text5");
 
 articleText.addEventListener('mousemove', () => {
-articleText.innerHTML = "З Новим роком !";
-articleText.style.color = "#e5d03d";
-articleText.style.left = "-198px";
-articleText.style.width = "655px";
-articleText.style.cursor = "pointer";
+  articleText.innerHTML = "З Новим роком !";
+  articleText.style.color = "#e5d03d";
+  articleText.style.left = "-198px";
+  articleText.style.width = "655px";
+  articleText.style.cursor = "pointer";
 });
 
 const accs = document.getElementsByClassName("contentBs");
@@ -51,13 +51,15 @@ accs[i].addEventListener("click", function() {
 const newCard = document.querySelector('.accordions');
 const newText = document.querySelector('.text-car2');
 const newPhoto2 = document.querySelector('.rabbit');
+const cardOff = document.querySelector('.cardOff');
 
-newCard.addEventListener('click', function loop() {
+newCard.addEventListener('click', function loop(/*event*/) {
+  //event.stopPropagation();//чтоб событие не всплывало 
   const newCar = document.querySelector('#newCar img');
-  newCar.style.cursor = "pointer";
-  newText.innerHTML = "Эта тачка тебе";
-  newText.style.color = "#ffeb00";
-  newText.style.left = "-84px";
+    newCar.style.cursor = "pointer";
+    newText.innerHTML = "Эта тачка тебе";
+    newText.style.color = "#ffeb00";
+    newText.style.left = "-84px";
   if (newCar !== null) { // если элемент найден
     newCar.setAttribute('src', 'https://gif5gif.ucoz.ru/_ph/1/130202818.gif?1665612335');
     return true;
@@ -65,12 +67,13 @@ newCard.addEventListener('click', function loop() {
     return false;
   }
 });
-newCar.addEventListener('mousemove', function loop() {
+newCar.addEventListener('mousemove', function loop(/*event*/) {
+  //event.stopPropagation();//чтоб событие не всплывало 
   const newCar = document.querySelector('#newCar img');
-  newCar.style.cursor = "pointer";
-   newText.innerHTML = "А эта тачка жене";
-   newText.style.color = "#e30d0d";
-   newText.style.left = "198px";
+    newCar.style.cursor = "pointer";
+    newText.innerHTML = "А эта тачка жене";
+    newText.style.color = "#e30d0d";
+    newText.style.left = "198px";
   if (newCar !== null) { // если элемент найден
     newCar.setAttribute('src', 'https://gif5gif.ucoz.ru/_ph/1/883246981.gif');
     return true;
@@ -79,9 +82,10 @@ newCar.addEventListener('mousemove', function loop() {
   }  
 });
 
-newCard.addEventListener('click', function loopp() {
-const newPhoto = document.querySelector('#newPhoto img');
-  newPhoto.style.cursor = "pointer";
+newCard.addEventListener('click', function loopp(/*event*/) {
+  //event.stopPropagation();//чтоб событие не всплывало 
+  const newPhoto = document.querySelector('#newPhoto img');
+    newPhoto.style.cursor = "pointer";
   if (newPhoto !== null) { // если элемент найден
     newPhoto.setAttribute('src', 'https://god-krolika.ru/wp-content/uploads/2022/06/snov8y923uu-25.gif');
     return true;
@@ -89,9 +93,10 @@ const newPhoto = document.querySelector('#newPhoto img');
     return false;
   }
 });
-newPhoto.addEventListener('mousemove', function loopp() {
+newPhoto.addEventListener('mousemove', function loopp(/*event*/) {
+  //event.stopPropagation();//чтоб событие не всплывало 
   const newPhoto = document.querySelector('#newPhoto img');
-  newPhoto.style.cursor = "pointer";
+    newPhoto.style.cursor = "pointer";
   if (newPhoto !== null) { // если элемент найден
     newPhoto.setAttribute('src', 'https://www.mirgif.com/prazdniki/noviy-god/nov-god-7.gif');
     return true;
@@ -100,9 +105,10 @@ newPhoto.addEventListener('mousemove', function loopp() {
   }
 });
 
-newCard.addEventListener('click', function looppp() {
-const newPhoto1 = document.querySelector('#newPhoto1 img');
-  newPhoto1.style.cursor = "pointer";
+newCard.addEventListener('click', function looppp(/*event*/) {
+  //event.stopPropagation();//чтоб событие не всплывало 
+  const newPhoto1 = document.querySelector('#newPhoto1 img');
+    newPhoto1.style.cursor = "pointer";
   if (newPhoto1 !== null) { // если элемент найден
     newPhoto1.setAttribute('src', 'https://miranimacii.ru/_ph/2/834869077.gif');
     return true;
@@ -110,9 +116,10 @@ const newPhoto1 = document.querySelector('#newPhoto1 img');
     return false;
   }
 });
-newPhoto1.addEventListener('mousemove', function looppp() {
+newPhoto1.addEventListener('mousemove', function looppp(/*event*/) {
+ //event.stopPropagation();//чтоб событие не всплывало 
   const newPhoto1 = document.querySelector('#newPhoto1 img');
-  newPhoto1.style.cursor = "pointer";
+    newPhoto1.style.cursor = "pointer";
   if (newPhoto1 !== null) { // если элемент найден
     newPhoto1.setAttribute('src', 'https://www.mirgif.com/prazdniki/noviy-god/novyj-god-180.gif');
     return true;
@@ -121,9 +128,10 @@ newPhoto1.addEventListener('mousemove', function looppp() {
   }
 });
 
-newPhoto2.addEventListener('click', function loopppp() {
-const newPhoto2 = document.querySelector('#newPhoto2 img');
-  newPhoto2.style.cursor = "pointer";
+newPhoto2.addEventListener('click', function loopppp(/*event*/) {
+  //event.stopPropagation();//чтоб событие не всплывало 
+  const newPhoto2 = document.querySelector('#newPhoto2 img');
+    newPhoto2.style.cursor = "pointer";
   if (newPhoto2 !== null) { // если элемент найден
     newPhoto2.setAttribute('src', 'https://2023god.com/wp-content/uploads/2022/09/animacionnye-otkrytki-s-novym-2023-godom-krolika-16-1.gif');
     return true;
@@ -131,9 +139,10 @@ const newPhoto2 = document.querySelector('#newPhoto2 img');
     return false;
   }
 });
-newPhoto2.addEventListener('mousemove', function loopppp() {
+newPhoto2.addEventListener('mousemove', function loopppp(/*event*/) {
+  //event.stopPropagation();//чтоб событие не всплывало 
   const newPhoto2 = document.querySelector('#newPhoto2 img');
-  newPhoto2.style.cursor = "pointer";
+    newPhoto2.style.cursor = "pointer";
   if (newPhoto2 !== null) { // если элемент найден
     newPhoto2.setAttribute('src', 'https://god-krolika.ru/wp-content/uploads/2022/06/6fed085f54bf2b5e385ceb6c2550a4b0.gif');
     return true;
@@ -151,20 +160,55 @@ newPhoto2.addEventListener('mousemove', function loopppp() {
 <img src="https://something.com/something.svg">
 </div> это пример, как нужно записывать img для замены ссылок */
 
+// можно нажимать на самого Деда Мороза /*photoOff*/
+cardOff.addEventListener('click', function showHide(/*event*/) {
+  //event.stopPropagation();//чтоб событие не всплывало 
+  const photoOff = document.querySelector('#photoOff img');
+    photoOff.style.cursor = "pointer";
+  if (photoOff.style.display == "block") { // если элемент найден // 
+    photoOff.style.display = "none";
+    return true;
+  } else { // иначе он не найден
+    photoOff.style.display = "block";
+    return false;
+  }
+  //alert("hi");
+});
+
 //5 пример с AdjacentHTML
 //создаем переменну с контентом, который будет в новом элементе 
-//<article></article>,который будет расположен над <article id="img"></article> 
+//<article></article>,который будет расположен над <img class"friends">
 const content = "Happy New Year !";
 document.querySelector(".friends").insertAdjacentHTML("beforebegin", 
-`<article class="text-friends"> ${content} </article>`);
-const textFriends = document.querySelector('.text-friends');
-textFriends.style ="font-size: 127px";
-textFriends.style.position ="absolute";
-textFriends.style.color = "#ff9800";
-textFriends.style.top = "2773px";
-textFriends.style.left = "161px";
-textFriends.style.zIndex = "1";
-textFriends.style.textShadow = "7px 7px 7px";
+`<article id="text-friends"> ${content} </article>`);
+const textFriends = document.querySelector('#text-friends');
+  textFriends.style ="font-size: 127px";
+  textFriends.style.position ="absolute";
+  textFriends.style.color = "#ff9800";
+  textFriends.style.top = "2773px";
+  textFriends.style.left = "161px";
+  textFriends.style.zIndex = "1";
+  textFriends.style.textShadow = "7px 7px 7px";
+
+function blink() {
+  const article = document.getElementById('text-friends');//#hi
+  if (article.style.visibility == "hidden") {
+    article.style.visibility = "visible";
+    //textFriends.style.color = "green"; изменить цвет текста здесь можно
+  } else {
+    article.style.visibility = "hidden";
+  }
+} 
+/*можно сделать в штмл див или артикль id с текстом hi, найти его 
+const article = document.getElementById('hi'); и включить сэт интервал*/
+window.setInterval(blink, 2000);
+
+/*const timer = setInterval(() => {// по идее работает если поставить ссылку с неограниченным колличеством фоток, на пример, 'https://picsum.photos/100'
+  const resultLoop = blink();
+  if (resultLoop === true) { // если элемент найден
+    clearInterval(timer); // остановить setInterval
+  }
+}, 3000);*/ 
 
 /*//3 пример создаем элемент
 const div = document.createElement("div");
