fefed3cd730007d05a54bed8e799cb23476ed930
Egnat <sergey_p_i@mail.ru>
2022-10-30 02:40:48 +0300

    (all)

--- script.js/main.js
+++ script.js/main.js
@@ -183,7 +183,7 @@ newPhoto2.addEventListener('mousemove', function loopppp(event) {
 cardOff.addEventListener('click', function showHide(event) {
   event.stopPropagation();//чтоб событие не всплывало 
   const photoOff = document.querySelector('.photoOff_santaInPipe img');
-    photoOff.style.cursor = "pointer";
+    //photoOff.style.cursor = "pointer";
   if (photoOff.style.display == "block") { // если элемент найден // 
     photoOff.style.display = "none";
     return true;
@@ -282,6 +282,57 @@ function blinkeded() {
     // Вызываем функцию раз в одну секунду
     setTimeout('timer()', 1000);/*setInterval работает тоже*/
 }
+
+//функция для перетаскивания элементы мышкой на экране
+const deer = document.querySelector('.deer');
+
+// Отключаем дефолтный DnD
+deer.ondragstart = () => false;
+
+// Функция для получение координат элемента
+const getCoords = (elem) => {
+  const box = elem.getBoundingClientRect();
+  return {
+    top: box.top + pageYOffset,
+    left: box.left + pageXOffset
+  };
+}
+
+ /*Основной механизм DnD*/ 
+// Этап 1. Вешаем обработчик на mousedown
+deer.addEventListener('mousedown', (e) => {
+  // Этап 2. Вычисляем начальные координаты положения элемента
+  const coords = getCoords(deer);
+  // console.log('coords', coords);
+  const shiftX = e.pageX - coords.left;
+  // console.log('shiftX', shiftX);
+  const shiftY = e.pageY - coords.top;
+  // console.log('shiftY', shiftY);
+  
+  // Этап 3. Подготовка к переносу
+  // Создаем функцию для изменения стилей и перемещения элемента
+  const moveAt = (e) => {
+    deer.style.left = e.pageX - shiftX + 'px';
+    deer.style.top = e.pageY - shiftY + 'px';
+  }
+  // Создаем функцию для удаления всех обработчиков
+  const theEnd = () => {
+    document.removeEventListener('mousemove', moveAt);
+    document.removeEventListener('mouseup', theEnd);
+  }
+
+  // Чтобы перенос работал, делаем элемент absolute
+  deer.style.position = 'absolute';
+  // Ставим первые значения переноса на точке нахождения элемента
+  moveAt(e);
+  deer.style.zIndex = 1000; // делаем над другими элементами
+  
+  // Этап 4. Двигаем элемент.
+  // При изменении положения мыши меняем координаты квадрата
+  document.addEventListener('mousemove', moveAt);
+  // Когда отпускаем мышь - все удаляем
+  document.addEventListener('mouseup', theEnd);
+});
 
 // Получает текущую дату и время
 //d = new Date();
